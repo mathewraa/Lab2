8fad9515fc34b8f61c8d92aaa6063180f48bcce6
Rebecca <rebecca@mathew.com>
2019-01-22 14:57:02 -0500

    messge

---
+++ Setup.java
@@ -0,0 +1,39 @@
+import bridges.connect.Bridges;
+import bridges.base.Array;
+import bridges.base.Element;
+
+public class Setup {
+	public static void main(String[] args) throws Exception {
+
+	    /* Initialize a Bridges connection with your credentials */
+	    /* TODO: plug your own BRIDGES credentials */
+	    Bridges bridges = new Bridges(0, "mathewra", "389826050341");
+
+	    /* Set an assignment title */
+	    bridges.setTitle("Array Example");
+
+	    /* Set up the array dimensions, allocate an Array of Elements */
+	    /* TODO: Make an array of size 10 */
+	    int arraySize = 10;
+	    Array<Integer> arr = new Array<Integer> (arraySize);
+	    
+	    /* Populate the array with integers */
+	    /* TODO: Make the array store square numbers*/
+	    
+	    for(int i=0; i<arr.getSize(); i++) {
+	    	
+	    	arr.getElement(i).setValue(i*i);
+	   
+
+	    	arr.getElement(i).setValue(i);
+	    /* set the value as a Label */
+	    	
+	    arr.getElement(i).setLabel(String.valueOf(i*i));
+	    }
+	    /* Tell BRIDGES which data structure to visualize */
+	    bridges.setDataStructure(arr);
+
+	    /* Visualize the Array */
+	    bridges.visualize();
+	}
+}
